d1b34cb5432c6b0c6751e4707b70bbc86e53a5ad
mulukou <mulukou@gmail.com>
2020-09-12 04:48:12 -0300

    Adicionado Script e outras configurações

---
+++ InstallEverything.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+cp -rf config ~/.config/
+yay -S visual-studio-code-bin i3-gaps termite polybar jgmenu dunst compton-tryone-git nitrogen gnome-disk-utility jupyter postman dbeaver lutris minecraft-technic-launcher aseprite flameshot whatsapp-nativefier qbittorrent filezilla discord_arch_electron teams chromedriver google-chrome mailspring spotify obs-studio popcorntime mon2cam-git ckb-next-git openrgb neofetch oh-my-zsh-git
