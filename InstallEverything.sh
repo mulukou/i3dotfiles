7437c1ce2c93b997c2fd962bb517eb1833603454
Rodrigo Moreira Ribeiro <rodrigo.ribeiro@ecotx.com.br>
2020-11-26 10:11:06 -0300

    atualizado 26/11/2020

--- InstallEverything.sh
+++ InstallEverything.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 cp -rf config ~/.config/
-yay -S visual-studio-code-bin i3-gaps termite polybar jgmenu dunst compton-tryone-git nitrogen gnome-disk-utility jupyter postman dbeaver lutris minecraft-technic-launcher aseprite flameshot whatsapp-nativefier qbittorrent filezilla discord_arch_electron teams chromedriver google-chrome mailspring spotify obs-studio popcorntime mon2cam-git ckb-next-git openrgb neofetch oh-my-zsh-git
+yay -S visual-studio-code-bin i3-gaps jupyter postman dbeaver lutris minecraft-technic-launcher aseprite flameshot whatsapp-nativefier filezilla discord_arch_electron teams chromedriver google-chrome mailspring spotify obs-studio popcorntime mon2cam-git ckb-next-git neofetch oh-my-zsh-git npm yarn nodejs ninja cmake
